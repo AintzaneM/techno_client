b535978c48102a7e0220d6dcb9bb993245641e24
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-15 21:03:29 +0100

    Home, Header, Login component

---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from "styled-components";
+import Login from './Login';
+
+const Home = (props) => {
+  return (
+    <Container>
+        Home
+          <Link href="/login">
+              {/* <img src="/images/home-icon.svg" alt="" /> */}
+              <span>Login</span>
+          </Link>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+position: relative;
+min-height: calc(100vh - 250px);
+overflow-x: hidden;
+display: block;
+top: 72px;
+padding: 0 calc(3.5vw + 5px);
+&:after {
+  background: url("/images/home-background.png") center center / cover
+    no-repeat fixed;
+  content: "";
+  position: absolute;
+  inset: 0px;
+  opacity: 1;
+  z-index: -1;
+}
+`;
+
+const Link = styled.a`
+// opacity: 1;
+height: 100%;
+width: 5vw;
+// z-index: 0;
+background-color: red;
+`
+
+
+export default Home
---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+const Login = () => {
+  return (
+    <Container>
+      Login Page
+      
+    </Container>
+  )
+}
+
+const Container = styled.div`
+color: red;
+`
+
+export default Login

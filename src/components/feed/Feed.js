420fb41de874e5793bb032897890ee683d130da8
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-17 16:29:50 +0100

    added share&post components

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Share from '../share/Share'
+import Post from '../post/Post'
 
 const Feed = () => {
   return (
-    <Container className="feed">Feed</Container>
+    <Container className="feed">
+      <FeedWrapper>
+        <Share/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+      </FeedWrapper>
+    </Container>
   )
 }
 
@@ -12,6 +22,11 @@ export default Feed
 
 const Container = styled.div`
 flex: 5.5;
+padding: 20px;
 
+
+`
+
+const FeedWrapper = styled.div`
 
 `

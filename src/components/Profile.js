6aabd76fd5998229fb98577bf54053d21113a0bf
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-28 17:40:44 +0100

    hook useParams

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,24 @@ import Sidebar from './sidebar/Sidebar'
 import Header from "./Header"
 import Feed from './feed/Feed'
 import Rightbar from './rightbar/Rightbar'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 
 const Profile = () => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({})
+  const username = useParams().username;
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      const res = await axios.get(`/users?username=${username}`)
+      setUser(res.data)
+
+    }
+    
+    fetchUser()
+  }, [username])
 
   return (
     <>
@@ -16,19 +31,21 @@ const Profile = () => {
         <div className='profile-right-container'>
           <div className='profile-right-top-container'>
             <div className='profile-cover-container'>
-              <img className="profile-cover-img" src="/images/paisaje.jpeg" alt=""></img>
-              <img className="profile-user-img" src={`${PF}/profile.jpg`} alt=""></img>
+              <img className="profile-cover-img" src={user.coverPicture || PF + "noCover.png"} alt=""></img>
+              <img className="profile-user-img" src={user.profilePicture || PF + "noAvatar.png"} alt=""></img>
             </div>
             <div className='profile-info'>
-              <h4 className='profile-info-name'>Jane Doe</h4>
-              <span className='profile-info-desc'>Helloooooooooooooo</span>
+              <h4 className='profile-info-name'>{user.username}</h4>
+              
+              <span className='profile-info-desc'>{user.description}</span>
 
             </div>
           </div>
          
           <div className='profile-right-bottom'>
-            <Feed />
-            <Rightbar profile/>
+            <Feed username= {username} />
+            <Rightbar user={user}/>
+            {console.log(user)}
           </div>
 
         </div>

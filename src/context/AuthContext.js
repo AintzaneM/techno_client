981c91d7d6d3e4ed1db3b7b7219e5af191f4b63c
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-03-02 20:10:09 +0100

    minor changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,18 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user: null,
+    user: {
+        _id: "621e6532d2cf1c00ab443448",
+        username:"test",
+        email:"test@gmail.com",
+        password:"$2b$10$MTUmUsh0uQVNLt/08J6Yk.tP5IbTqqo8LZ8uwU8WfSlk7DqV5e.XG",
+        profilePicture:"",
+        coverPicture:"",
+        followers: [],
+        followings: [
+            
+        ]
+    },
     isFetching: false,
     error: false
 };

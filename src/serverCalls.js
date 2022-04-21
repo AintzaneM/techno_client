6103248fa0f66bfb53eff064bdfdf6fdc9c93c0e
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-04-21 14:03:21 +0200

    server call login

--- src/serverCalls.js
+++ src/serverCalls.js
@@ -1,13 +1,14 @@
 import axios from "axios"
 
 export const loginCall = async (userCredential, dispatch) => {
-    dispatch({type: "LOGIN_START"});
+    dispatch({ type: "LOGIN_START" });
     try {
         const res = await axios.post("/auth/login", userCredential);
-        dispatch({type: "LOGIN_SUCCESS", payload: res.data})
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
         console.log(res.data, "dataa")
-    }catch(err){
-        dispatch({type: "LOGIN_FAILURE", payload: err})
+    } catch (err) {
+        dispatch({ type: "LOGIN_FAILURE", payload: err })
 
     }
 }
+
